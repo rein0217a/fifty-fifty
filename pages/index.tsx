9e1eeec05452387ce70f4d76e716352c738e7654
Reina <rein0217a@icloud.com>
2022-04-05 18:05:27 +0900

    add:ファイルアップロード、計算結果出力機能作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
-import type { NextPage, GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 import CSVUpload from '../components/CSVUpload'
 
-type Props = {
-  data: {name:string}
-}
 
-const Upload = ({data}: Props) => {
+const Upload = () => {
   console.log('Upload')
   return (
     <Layout>
       <CSVUpload />
-      {/* {data.name} */}
     </Layout>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  // 外部のAPIエンドポイントを呼び出してpostsを取得します。
-  // 任意のデータ取得ライブラリを使用できます。
-  const res = await fetch('http://localhost:4000/api/hello');
-  const data = await res.json();
-  console.log('getStaticProps')
-  return {
-    props: {
-      data
-    }
-  };
-}
 
 export default Upload
